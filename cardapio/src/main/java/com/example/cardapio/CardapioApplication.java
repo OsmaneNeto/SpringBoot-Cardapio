de44f21e9695630a95a2300e2e02ecf9fd31ed48
osmane <osmane.neto@hotmail.com>
2025-01-08 11:52:11 -0300

    Aplicação sprigboot para cardápio digital

---
+++ cardapio/src/main/java/com/example/cardapio/CardapioApplication.java
@@ -0,0 +1,15 @@
+package com.example.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.Getter;
+import lombok.Setter;
+
+@SpringBootApplication //aentrada do spring
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class, args);
+	}
+
+}
